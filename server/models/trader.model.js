b25418ab0b636359d55ae07af8e237fb42f9749a
Tranzorro <shiekadam@gmail.com>
2022-04-19 21:10:29 -0400

    making modifications to db and trader json file
    attempting to set up mongo db model of trader to match trader.json file for easier implimentation of data storing and insertion from jsons.

--- server/models/trader.model.js
+++ server/models/trader.model.js
@@ -3,26 +3,63 @@ const  mongoose = require("mongoose");
 
 const TradersSchema = new mongoose.Schema({
 
-    traderName:{
-        traderProfileImage:String,
-        traderName: String,
+    npc:
+    [
+        {
+            name:String,
+            imageUrl:String
+        },
+        {
+            trades:
+            [
+                {
+                    wants:
+                    [
+                        {
+                            itemName:String,
+                            itemTooltip:String,
+                            itemImageUrl:String,
+                            itemCount:Number
+                        }
+                    ],
+                    gives:
+                    [
+                        {
+                            itemName:String,
+                            itemTooltip:String,
+                            itemImageUrl:String,
+                            itemCount:Number
+                        }
+                    ],
+                }
+            ]
+        },
+    ]
+
+
+
+
+// trying something below. ignore it.
+
+    // traderName:{
+        // traderProfileImage:String,
+        // traderName: String,
         // message: "hmmmm name",
         // minlength: [4, "more than 4 characters please."],
         // required: [true, "need a name duh."],
-    },
+    // },
 
-    tradesContent: [
-        {
-            wantName:String,
-            giveName:String,
-            wantItemCount:Number,
-            giveItemCount:Number,
-            wantImageUrl:String,
-            giveImageUrl:String,
-            cssClassName:String,
-            // isWant:Boolean
-        }
-],
+    // tradesContent: [
+        // {
+            // wantName:String,
+            // giveName:String,
+            // wantItemCount:Number,
+            // giveItemCount:Number,
+            // wantImageUrl:String,
+            // giveImageUrl:String,
+            // cssClassName:String,
+        // }
+// ],
     // gives: {
     //     type: String,
     //     required: [true, "what you want?"],
